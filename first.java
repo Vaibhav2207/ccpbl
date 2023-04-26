660414eb8d3eed5323d1d66d155768116b79b87e
Vaibhav2207 <boratevp@gmail.com>
2023-04-26 14:45:00 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,9 @@
+class first {
+    public static String welcome() {
+        return "Welcome.....<3";
+    }
+
+    public static void main(String args[]) {
+        System.out.println(welcome());
+    }
+}
